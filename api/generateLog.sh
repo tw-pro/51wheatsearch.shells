32d799a264f6cc6153e4de71b73f5a59e6989c6c
Timlo <1159193863@qq.com>
2020-08-19 20:01:46 +0800

    20200819 修复逻辑
    20200819 修复逻辑

--- api/generateLog.sh
+++ api/generateLog.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
+
+# 51wheatsearch 项目
+
+# 创建 api.51wheatsearch.api 日志文件
+echo '' > /data/api.51wheatsearch.com/api/logs/access.pipe &&
+# 创建 api.51wheatsearch.gs 日志文件
+echo '' > /data/api.51wheatsearch.com/gs/logs/access.pipe &&
+# 创建 api.51wheatsearch.admin 日志文件
+echo '' > /data/api.51wheatsearch.com/admin/logs/access.pipe &&
+
+
+
+# twowen-xxx项目
 LOGDATE=$(date +%Y%m%d) &&
-# 创建 twowen-api 日志文件
+# 清空 twowen-api 今日日志文件
 touch /data/srv/twowen-api/logs/${LOGDATE}-access.pipe &&
 chown -R nginx:nginx /data/srv/twowen-api/logs/ &&
 # 创建 twowen-gs 日志文件
@@ -9,12 +22,3 @@ chown -R nginx:nginx /data/srv/twowen-gs/logs/ &&
 # 创建 twowen-admin 日志文件
 touch /data/srv/twowen-admin/logs/${LOGDATE}-access.pipe &&
 chown -R nginx:nginx /data/srv/twowen-admin/logs/ &&
-# 创建 api.51wheatsearch.api 日志文件
-touch /data/api.51wheatsearch.com/api/logs/${LOGDATE}-access.pipe &&
-chown -R nginx:nginx /data/api.51wheatsearch.com/api/logs/ &&
-# 创建 api.51wheatsearch.gs 日志文件
-touch /data/api.51wheatsearch.com/gs/logs/${LOGDATE}-access.pipe &&
-chown -R nginx:nginx /data/api.51wheatsearch.com/gs/logs/ &&
-# 创建 api.51wheatsearch.admin 日志文件
-touch /data/api.51wheatsearch.com/admin/logs/${LOGDATE}-access.pipe &&
-chown -R nginx:nginx /data/api.51wheatsearch.com/admin/logs/
