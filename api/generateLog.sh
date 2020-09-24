d368dd1706798447a20bb674b1acd578c6209f9e
Timlo <1159193863@qq.com>
2020-09-24 10:15:20 +0800

    Update generateLog.sh

--- api/generateLog.sh
+++ api/generateLog.sh
@@ -8,11 +8,13 @@ LOGDATE=$(date +%Y%m%d) &&
 LOGDIR=/data/api.51wheatsearch.com/api/logs/ &&
 zip -q -9 ${LOGDIR}${LOGDATE}-access.pipe.zip ${LOGDIR}access.pipe &&
 echo '' > ${LOGDIR}access.pipe &&
+
 # 压缩清空 api.51wheatsearch.gs 日志文件
 LOGDIR=/data/api.51wheatsearch.com/gs/logs/ &&
 zip -q -9 ${LOGDIR}${LOGDATE}-access.pipe.zip ${LOGDIR}access.pipe &&
 echo '' > ${LOGDIR}access.pipe &&
+
 # 压缩清空 api.51wheatsearch.admin 日志文件
 LOGDIR=/data/api.51wheatsearch.com/admin/logs/ &&
 zip -q -9 ${LOGDIR}${LOGDATE}-access.pipe.zip ${LOGDIR}access.pipe &&
-echo '' > ${LOGDIR}access.pipe &&
+echo '' > ${LOGDIR}access.pipe
